9ade5872b02435772179140fb0a4f8f6807d151c
saheb singh <sahebsingh@sahebs-MacBook-Pro.local>
2015-04-21 09:07:25 +0530

    Initial Commit

---
+++ factory design pattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  factory design pattern
+//
+//  Created by saheb singh on 21/04/15.
+//  Copyright (c) 2015 saheb singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ factory design pattern/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  factory design pattern
+//
+//  Created by saheb singh on 21/04/15.
+//  Copyright (c) 2015 saheb singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
